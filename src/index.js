19ff0e3990da73a6756415c68ca2f87d8a61dc25
PierreLebedel <pierrelebedel@gmail.com>
2025-03-11 09:26:34 +0100

    inline tool button style

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ export default class EmojiPickerTool {
     render() {
         this.button = document.createElement('button');
         this.button.type = 'button';
+        this.button.classList.add(this.api.styles.inlineToolButton);
         this.button.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M12 12m-9 0a9 9 0 1 0 18 0a9 9 0 1 0 -18 0" /><path d="M9 10l.01 0" /><path d="M15 10l.01 0" /><path d="M9.5 15a3.5 3.5 0 0 0 5 0" /></svg>';
 
         return this.button;
